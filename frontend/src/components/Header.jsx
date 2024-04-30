a8907a98206523b377c0d10c2b5c029091fdd52c
shailendra singh shekhawat <shelbyquiz0@gmail.com>
2024-04-30 23:58:50 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -34,7 +34,7 @@ const Header = () => {
           <LinkContainer to='/'>
             <Navbar.Brand>
               <img src={logo} alt='PoPshop' />
-              PoPShop
+              CommerceCraft
             </Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
